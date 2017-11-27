2ab3d4f9398a3633e719a7141f5cda2360e54c89
dingyasong <dingys@zorrogps.com>
2017-11-27 17:14:08 +0800

    change gradle version

--- app/src/main/java/com/dang/dangmap/MainActivity.java
+++ app/src/main/java/com/dang/dangmap/MainActivity.java
@@ -3,20 +3,16 @@ package com.dang.dangmap;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
-    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         mContext = this;
-        mMapView = (MapView) findViewById(R.id.bmapView);
+//        mMapView = (MapView) findViewById(R.id.bmapView);
         //add note
     }
 
@@ -24,20 +20,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
+//        mMapView.onDestroy();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
+//        mMapView.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
+//        mMapView.onPause();
     }
 }
